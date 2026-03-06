a657fdbb5af32782bca44c81e5f2cf15f98120ff
avelickovic <aleksa.velickovic@orioninc.com>
2026-03-06 16:23:58 +0000

    added buildRegisterMessage and cli for client

--- client/src/client_cli.cpp
+++ client/src/client_cli.cpp
@@ -2,30 +2,29 @@
 #include <iostream>
 
 int main() {
+    std::string server_ip = "127.0.0.1";
+    int server_port = 5060;
 
-   // SipClient client;
+    SIPClient client(server_ip, server_port);
 
-    while(true) {
-
+    while (true) {
         std::string cmd;
         std::cout << "> ";
         std::cin >> cmd;
 
-        if(cmd == "register") {
-
-            std::string username, domain, ip;
-            int port;
-
+        if (cmd == "register") {
+            std::string username, domain;
             std::cout << "username: ";
             std::cin >> username;
-
             std::cout << "domain: ";
             std::cin >> domain;
 
             //client.sendRegister(username, domain);
+            std::string message=client.buildRegisterMessage(username, domain);
+            std::cout << message << std::endl;
         }
 
-        if(cmd == "exit")
+        if (cmd == "exit")
             break;
     }
 }
