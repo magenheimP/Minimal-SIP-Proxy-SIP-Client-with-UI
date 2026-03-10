e5e2949af7a54a539e0749a67a2f555d85a588da
PetarMagenheim <petar.magenhajm@orioninc.com>
2026-03-10 15:43:35 +0100

    quick fix, merge conflict over UI didnt save the changes

--- common/include/common/sip_message.hpp
+++ common/include/common/sip_message.hpp
@@ -18,11 +18,9 @@ namespace common {
         std::string get_header(const std::string& name) const;
         void set_header(const std::string& name, const std::string& value);
         void add_header(const std::string& name, const std::string& value);
-<<<<<<< feature/loggerSkeleton
 
         std::string serialize() const;
-=======
->>>>>>> dev
+
     };
 
 }
