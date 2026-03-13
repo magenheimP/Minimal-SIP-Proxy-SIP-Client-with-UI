bf38d0d5938be2f8556bf8f0282f4c8acdd2556d
tamara smolcic <tamara.smolcic@orioninc.com>
2026-03-13 15:53:08 +0000

    Implemented basic SIPRouter and improved SIP parsing and registration handling

--- proxy/src/registration_table.cpp
+++ proxy/src/registration_table.cpp
@@ -3,20 +3,55 @@
 //
 
 #include "../include/proxy/registration_table.hpp"
+#include "../../common/include/common/logger.hpp"
+
+#include <mutex>
+#include <shared_mutex>
 
 namespace proxy {
 
 void RegistrationTable::register_user(const std::string& user,
                                       const std::string& contact) {
-  registrations_[user] = contact;
+    //  unique_lock for write operations
+    std::unique_lock lock(mutex_);
+
+    registrations_[user] = contact;
+
+    common::Logger::instance("proxy.log").log(
+        "RegistrationTable",
+        "",
+        "REGISTERED",
+        "Stored registration for user = " + user + ", contact = " + contact);
 }
 
-std::string RegistrationTable::get_contact(const std::string& user) const {
-  auto it = registrations_.find(user);
-  if (it != registrations_.end()) {
-    return it->second;
-  }
-  return "";
+std::optional<std::string> RegistrationTable::get_contact(const std::string& user) const {
+
+    //  shared_lock for read operations
+    std::shared_lock lock(mutex_);
+
+    auto it = registrations_.find(user);
+    if (it != registrations_.end()) {
+        common::Logger::instance("proxy.log").log(
+            "RegistrationTable",
+            "",
+            "LOOKUP_OK",
+            "Found contact for user = " + user + ", contact = " + it->second);
+
+        return it->second;
+    }
+
+    common::Logger::instance("proxy.log").log(
+        "RegistrationTable",
+        "",
+        "LOOKUP_FAIL",
+        "User not found in registration table: user = " + user);
+
+    return std::nullopt;
+}
+
+bool RegistrationTable::is_registered(const std::string& user) const {
+    std::shared_lock lock(mutex_);
+    return registrations_.find(user) != registrations_.end();
 }
 
 }
